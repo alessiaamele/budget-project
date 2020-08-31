835c23afa3d4c876fb20eba9a5582b4c3063f620
alessia M <alessia.mele.097@gmail.com>
2020-08-31 17:34:15 +0100

    modified gitignore file, added new dependencies in pom.xml and created a new entity Days

--- src/main/java/com/qa/budgetproject/domain/Days.java
+++ src/main/java/com/qa/budgetproject/domain/Days.java
@@ -1,8 +1,41 @@
 package com.qa.budgetproject.domain;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 
 @Entity
 public class Days {
 
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @Column
+    private String day_of_week;
+
+    public Days(){
+
+    }
+
+    public Days(String day_of_week) {
+        this.day_of_week = day_of_week;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDay_of_week() {
+        return day_of_week;
+    }
+
+    public void setDay_of_week(String day_of_week) {
+        this.day_of_week = day_of_week;
+    }
 }
