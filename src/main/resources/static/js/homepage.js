7d5eb8e3a2f0cb33b910412b9b7c72f660401999
Alessia M <alessia.mele.097@gmail.com>
2020-09-03 19:27:32 +0100

    adapted homepage.js and homepage.html

--- src/main/resources/static/js/homepage.js
+++ src/main/resources/static/js/homepage.js
@@ -1,17 +1,37 @@
-function greeting(){
+function displayFoods(){
     const req = new XMLHttpRequest();
     req.onreadystatechange = () => {
         // Example handle logic
         if (req.status === 200 && req.readyState === 4) {
             if (req.getResponseHeader("Content-Type") === "application/json") {
                 console.log("oh look its some JSON: " + req.responseText);
-                // adding an element to the body example
-                 let elem = document.createElement('div');
-                 elem.textContent = "hello world";
-                 document.body.appendChild(elem);
+                //adding an element to the body example
+                let elem = document.createElement('div');
+                elem.textContent = "hello world";
+                document.body.appendChild(elem);
 
+                let stuff = JSON.parse(req.response);
+                stuff.forEach(el => {
+                    // console.log(el); // prints whole element
+                    // console.log(el.name); // allows access to specific value
 
-
+                    // adding title to the body of the page
+                    let elem = document.createElement('div');
+                    let header = document.createElement('h1');
+                    header.textContent = "Meal name: " + el.name;
+                    elem.appendChild(header);
+                    el.foods.forEach(food => {
+                        console.log(food) // print all notes for each notebook
+                        let title = document.createElement('p');
+                        let cost = document.createElement('p');
+                        title.textContent = "Ingredient: " + food.title;
+                        cost.textContent = "Cost: " + food.cost;
+                        elem.appendChild(title);
+                        elem.appendChild(cost);
+                    })
+                    //always do this otherwise it won't be added
+                    document.body.appendChild(elem);
+                });
             } else {
                 console.log(
                     "Looks like its not JSON but lets see what it is... " + req.responseText
@@ -21,6 +41,6 @@ function greeting(){
             console.log("Oh no... handle error");
         }
     };
-    req.open("GET", "http://localhost:8080/getAllNoteBooks");
+    req.open("GET", "http://localhost:8080/getAllMeals");
     req.send();
 }
