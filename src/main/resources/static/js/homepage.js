0d7aec7cc5fe2a7ce8e3e8d203cd93f9673258aa
Alessia M <alessia.mele.097@gmail.com>
2020-09-03 22:51:33 +0100

    made the input configuration on the homepage.js and greeting.html

--- src/main/resources/static/js/homepage.js
+++ src/main/resources/static/js/homepage.js
@@ -44,3 +44,30 @@ function displayFoods(){
     req.open("GET", "http://localhost:8080/getAllMeals");
     req.send();
 }
+
+function submitFood(){
+    let elements = document.getElementById("ingredientForm").elements;
+    // create a collection in json format and iterate over it to populate it
+    let obj ={};
+    for(let i = 0 ; i < elements.length - 1 ; i++){
+        let item = elements.item(i);
+        obj[item.name] = item.value;
+    }
+
+    const req = new XMLHttpRequest();
+    req.open("POST", "http://localhost:8080/createFood");
+    req.onload = () => {
+        if (req.status === 200 && req.readyState == 4) {
+            console.log("Server Responded with: " + req.responseText);
+        } else {
+            console.log("Oops...");
+        }
+    };
+    req.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+    req.send(JSON.stringify({ name: obj.name,
+        cost: obj.cost,
+        meal:{
+        id: Number(obj.mealId)}
+    }
+    ));
+}
