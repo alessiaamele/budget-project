847d45ef16d669a779cdf209a7d067c4d371fea2
Alessia M <alessia.mele.097@gmail.com>
2020-09-04 23:00:02 +0100

    added the update on javascript

--- src/main/resources/static/js/homepage.js
+++ src/main/resources/static/js/homepage.js
@@ -72,3 +72,31 @@ function submitFood(){
     }
     ));
 }
+
+function updateFood(){
+    let elements = document.getElementById("updateForm").elements;
+    // create a collection in json format and iterate over it to populate it
+    let obj ={};
+    for(let i = 0 ; i < elements.length - 1 ; i++){
+        let item = elements.item(i);
+        obj[item.name] = item.value;
+    }
+
+    const URL = "http://localhost:8080/updateFood/"+obj.mealId
+    const req = new XMLHttpRequest();
+    req.open("PUT", URL);
+    req.onload = () => {
+        if (req.status === 200 && req.readyState == 4) {
+            console.log("Server Responded with: " + req.responseText);
+        } else {
+            console.log("Oops...");
+        }
+    };
+    req.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+    req.send(JSON.stringify({ name: obj.name,
+            cost: obj.cost,
+            meal:{
+                id: Number(obj.mealId)}
+        }
+    ));
+}
